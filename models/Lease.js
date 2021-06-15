7c66882d4810a3fafe80b77fd2520c576f1f316e
Carmen <carmen.castillo19@gmail.com>
2021-06-15 12:09:35 -0500

    Update Lease
    add fields lease

--- models/Lease.js
+++ models/Lease.js
@@ -1,14 +1,16 @@
 const { Schema, model } = require('mongoose');
 
 const leaseSchema = new Schema({
+    name: String,
     status: String,
     startDate: Date,
     endDate: Date,
+    rentalRate: Number,
     contactId: {
         type: Schema.Types.ObjectId,
         ref: "Contact"
     },
-    propertieId: {
+    propertyId: {
         type: Schema.Types.ObjectId,
         ref: "Property"
     },
